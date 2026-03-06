893f423eda51a7d16a7a29b50be35239bfde5450
guilhermyfagundes-collab <guilhermy.fagundes@aluno.senai.br>
2026-03-06 11:01:42 -0300

    arrumei

--- exemplo03.php
+++ exemplo03.php
@@ -9,7 +9,7 @@ $funcionarios[0] = array('Joao filho', 1000, 100);
 $funcionarios[1] = array('Maria rute', 2000, 200);
 $funcionarios[2] = array('Jose salgado', 3000, 300);
 
-for ($ = 0; $i < 3; $i++) {
+for ($i = 0; $i < 3; $i++) {
     $nome = $funcionarios[$i][0];
     $desconto = calcularDesconto($funcionarios[$i][1], $funcionarios[$i][2]);
     echo "O Valor do desconto de $nome é de R$ $desconto. <br>";
