1875e1acdfdda4efeb69e4efcc9feda4db6fda1d
guilhermyfagundes-collab <guilhermy.fagundes@aluno.senai.br>
2026-03-06 09:54:50 -0300

    feito

--- exemplo01.php
+++ exemplo01.php
@@ -1,16 +1,16 @@
 <?php 
 
-$joaoSalalrio = 1000;
+$joaoSalario = 1000;
 $joaoPrevidencia = 100;
 $joaoNome = 'João Filho';
 $joaoDescontos = number_format($joaoSalario*0.275 + $joaoPrevidencia, 2, ',', '.');
 
-$mariaSalalrio = 2000;
+$mariaSalario = 2000;
 $mariaPrevidencia = 200;
 $mariaNome = 'Maria Rute';
 $mariaDescontos = number_format($mariaSalario*0.275 + $mariaPrevidencia, 2, ',', '.');
 
-$joseSalalrio = 3000;
+$joseSalario = 3000;
 $josePrevidencia = 300;
 $joseNome = 'José Salgado';
 $joseDescontos = number_format($joseSalario*0.275 + $josePrevidencia, 2, ',', '.');
