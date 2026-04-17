a25d15f4ee9560c96d189a684c1c9cd471b42edf
guilhermyfagundes-collab <guilhermy.fagundes@aluno.senai.br>
2026-04-17 08:47:07 -0300

    so atualizei

--- carro.php
+++ carro.php
@@ -14,8 +14,9 @@ class Carro {
     public function setVelocidade($novaVelocidade){
         if ($novaVelocidade >= 0 && $novaVelocidade <= 200) {
             $this->velocidade = $novaVelocidade;
+            echo "continue nesta Velocidade<br>";
         } else {
-            echo "Velocidade Invalida : $novaVelocidade<br>";
+            echo "Velocidade Invalida <br>";
         }
     }
 }
